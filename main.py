2b8fac5bc046f0d1a8f3b97c7cd38fe31ce5f964
JafiaAlvesMelo <jafiaa59@gmail.com>
2026-06-16 10:38:16 -0300

    Adicionei ao elif da escolha igual a 3 um menu, para depois iniciar as funções do CRUD de registros

--- main.py
+++ main.py
@@ -1,33 +1,70 @@
 import sys
 import os
+from banco import inicializar_banco
+from registros_diarios import criar_registro, ler_registros, ler_um, atualizar_registro, deletar_registro
+from relatorio import menu_relatorios
+
 
 sys.path.insert(0, os.path.dirname(__file__)) 
 
-from banco import inicializar_banco
+
+inicializar_banco()
 
 def menu_principal():
-    print("_" * 25)
-    print(" Sistema de Habitos Saudaveis ")
-    print("-" * 25)
+    while True:
+        print("_" * 35)
+        print(" Sistema de Habitos Saudaveis ")
+        print("-" * 35)
+        print(" 1 - Dados do Usuario")
+        print(" 2 - Dados de Habitos")
+        print(" 3 - Registros Diários ")
+        print(" 4 - Relatório")
+        print(" 5 - Sair")
+        escolha = input(" Escolha uma opção: ")
 
-while True:
-    print(" 1 - Cadastrar Usuario")
-    print(" 2 - Cadastrar Habitos")
-    print(" 3 - Registros Diarios ")
-    print(" 4 - Relatorio")
-    print(" 5 - Sair")
-    escolha = input(" Escolha uma opção: ")
+        if escolha == "1":
+          print("\nUsuario") 
 
-    if escolha == "1":
-      print(" Usuario")
-    elif escolha == "2":
-      print(" Habitos")
-    elif escolha == "3":
-      print(" Registros Diarios")
-    elif escolha == "4":
-      print(" Relatorio")
-    elif escolha == "5":
-      print(" Saindo do sistema")
-      break
-    else:
-      print(" Opção inválida. Tente novamente.") 
+        elif escolha == "2":
+          print("\nHabitos")
+
+        elif escolha == "3":
+          print("\n================= Registros Diarios =================\n")
+          print(" 1 - Criar novo registro")
+          print(" 2 - Ler todos os registros")
+          print(" 3 - Ler um registro específico")
+          print(" 4 - Atualizar registro")
+          print(" 5 - Deletar um registro")
+          print(" 6 - Voltar")
+          print("\n=====================================================\n")
+          opcao = int(input("Escolha uma opção: "))
+          if opcao == 1:
+             criar_registro()
+          elif opcao == 2:
+             ler_registros()
+          elif opcao == 3:
+             ler_um()
+          elif opcao == 4:
+             atualizar_registro()
+          elif opcao == 5:
+             deletar_registro()
+          elif opcao == 6:
+             print("Voltando ao menu principal.")
+             break
+          else:
+             print("Opção inválida!")
+             
+
+        elif escolha == "4":
+          print("\nRelatorio")
+          menu_relatorios()
+
+        elif escolha == "5":
+          print("\nSaindo do sistema")
+
+          break
+        else:
+          print(" Opção inválida. Tente novamente.") 
+
+if __name__ == "__main__":
+    menu_principal()
